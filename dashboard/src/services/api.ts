fb3410cba60e1c97e44ee39b3df06c7b0345d031
unknown <abdulahad010274@gmail.com>
2026-08-15 12:35:02 -0700

    updated

--- dashboard/src/services/api.ts
+++ dashboard/src/services/api.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://api.abdulahad.run.place/api';
 
 const request = async (url: string, options: RequestInit = {}) => {
   try {
--- portfolio/src/services/api.js
+++ portfolio/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://api.abdulahad.run.place/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
